6f64c71330c14546fa15c40dc49d19842c8af516
Colin D <colinv120@gmail.com>
2019-07-18 21:29:15 -0600

    fixed a bracket

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,35 +5,35 @@
 // ON CLICK NEEDS TO WRAP ALL THIS
 // on.("click") function associated with the Search Button
 $("#run-search").on("click", function(event) {
+  var apiKey = "d7GAhlX34Aala8VFlo2pjAppXv8PbR3I";
+  //pull the search term
+  var searchTerm = "animals";
+  // pull the start year
+  var sYear = "2012" + "0101";
+  //pull the end year
+  var eYear = "2012" + "1231";
+  var userAmount = 6;
 
-var apiKey = "d7GAhlX34Aala8VFlo2pjAppXv8PbR3I";
-//pull the search term
-var searchTerm = "animals";
-// pull the start year
-var sYear = "2012" + "0101";
-//pull the end year
-var eYear = "2012" + "1231";
-var userAmount = 6;
+  var queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    searchTerm +
+    "&facet_fields=source&facet=true&begin_date=" +
+    sYear +
+    "&end_date=" +
+    eYear +
+    "&api-key=" +
+    apiKey;
 
-var queryURL =
-  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-  searchTerm +
-  "&facet_fields=source&facet=true&begin_date=" +
-  sYear +
-  "&end_date=" +
-  eYear +
-  "&api-key=" +
-  apiKey;
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(dataset) {
-  var numRecords = dataset.response.docs.length;
-  var articleArray = [];
-  for (i = 0; i < userAmount; i++) {
-    article = dataset.response.docs[i];
-  }
-  console.log(response);
-  console.log(numRecords);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(dataset) {
+    var numRecords = dataset.response.docs.length;
+    var articleArray = [];
+    for (i = 0; i < userAmount; i++) {
+      article = dataset.response.docs[i];
+    }
+    console.log(response);
+    console.log(numRecords);
+  });
 });

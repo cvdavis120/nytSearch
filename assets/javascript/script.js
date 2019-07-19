3780efd19a2a8ff9da11cd43d738526af404c3db
Colin D <colinv120@gmail.com>
2019-07-18 21:04:53 -0600

    Started the ajax call with variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,25 @@
 //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
 //fq=source:("The New York Times")
-var searchTerm = "animals";
+//https://api.nytimes.com/svc/search/v2/articlesearch.json?q=animals&api-key=d7GAhlX34Aala8VFlo2pjAppXv8PbR3I
+
+// ON CLICK NEEDS TO WRAP ALL THIS
+
 var apiKey = "d7GAhlX34Aala8VFlo2pjAppXv8PbR3I";
 var queryURL =
   "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
   searchTerm +
-  "&fq=source:('The New York Times')&api-key=" +
+  "&api-key=" +
   apiKey;
 
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
+}).then(function(dataset) {
+  var numRecords = dataset.response.docs.length;
+  var searchTerm = "animals";
+
+  var sYear = "2005";
+  var eYear = "2010";
   console.log(response);
+  console.log(numRecords);
 });
